4654074c6448045e3882d832504d2239efe65265
Paul Brian Marcuelo <marcuelo1@gmail.com>
2022-08-23 20:54:33 +0800

    setup npms and index view for birthday

---
+++ src/app/feature/birthday22/views/indexView.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Birthday22IndexView extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <h1>hello</h1>
+    );
+  }
+}
+ 
+export default Birthday22IndexView;
